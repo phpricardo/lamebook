3641bc346c15a2ecfd491d8761b65c5affb4bf1e
Ricardo Lima <phpricardo@gmail.com>
2023-03-01 21:03:51 -0300

    corrige persistencia do like no banco

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  resources :posts do
-    # member do
-    #   post 'like'
-    # end
-  end
+  resources :posts
 
   post 'posts/:id/like', to: 'posts#like', as: :like
 end
